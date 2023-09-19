ea4ccdb78a44479d8c0ec05ab93f811e8551a8ab
samgdesign <samgilbert@sjp.com>
2023-09-18 23:02:56 -0400

    Mobile styles
    First push of mobile styles. Some pieces broken within navigation and sliders.

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,22 @@ $(".contribute input:checkbox").click(function () {
   var bol = $("input:checkbox:checked").length >= 5;
   $("input:checkbox").not(":checked").attr("disabled", bol);
 });
+
+$(function () {                       //run when the DOM is ready
+  $("#nav-button").click(function () {  //use a class, since your ID gets mangled
+    $(".top-container").toggleClass("expanded");      //add the class to the clicked element
+  });
+});
+
+
+$("[data-trigger]").on("click", function () {
+  var trigger_id = $(this).attr('data-trigger');
+  $(trigger_id).toggleClass("show");
+  $('body').toggleClass("offcanvas-active");
+});
+
+// close button 
+$(".btn-close").click(function (e) {
+  $(".navbar-collapse").removeClass("show");
+  $("body").removeClass("offcanvas-active");
+}); 
